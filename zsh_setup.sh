8fc23b455058d68f8a561b6372ced9aa8de4eacc
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-07-06 16:12:05 +0100

    fix: update zsh_config script to copy .zshrc to home directory

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -3,5 +3,6 @@ sudo pacman -Sy zsh
 zsh
 chsh -s `which zsh`
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo rm ~/.zshrc && cp .zshrc ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
