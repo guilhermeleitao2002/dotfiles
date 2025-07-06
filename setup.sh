bfb046687e8388e9ba7a893bb4cdfad96334f380
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-07-06 16:07:10 +0100

    change: separate zsh installation process from main setup

--- setup.sh
+++ setup.sh
@@ -14,13 +14,7 @@ GREP_COLORS='mt=1;36' echo "[+] backgrounds stowed"
 GREP_COLORS='mt=1;36' echo "\n Configuring kitty terminal"
 stow kitty
 GREP_COLORS='mt=1;36' echo "[+] kitty stowed"
-GREP_COLORS='mt=1;36' echo "Installing zsh"
-sudo pacman -Sy zsh
-zsh
-chsh -s `which zsh`
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+./zsh_setup.sh # Run separate script
 
 # Status bar configuration
 GREP_COLORS='mt=1;36' echo "\n Configuring waybar status bar"
---
+++ zsh_setup.sh
@@ -0,0 +1,7 @@
+GREP_COLORS='mt=1;36' echo "Installing zsh"
+sudo pacman -Sy zsh
+zsh
+chsh -s `which zsh`
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
