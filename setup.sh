5be5a749ad79a9963b126e05591c560ebc386df5
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-07-06 15:49:08 +0100

    feat: add terminal configuration

--- setup.sh
+++ setup.sh
@@ -3,11 +3,21 @@ GREP_COLORS='mt=1;36' echo "Installing GNU stow"
 yay -Sy stow
 
 # Wallpaper configuration
-GREP_COLORS='mt=1;36' echo "\n Configuring hyprpaperbackgrounds"
+GREP_COLORS='mt=1;36' echo "\n Installing hyprpaper"
 yay -Sy hyprpaper
 stow hyprpaper
-GREP_COLORS='mt=1;36' echo "hyprpaper stowed"
-GREP_COLORS='mt=1;36' echo "backgrounds stowed"
+GREP_COLORS='mt=1;36' echo "[+] hyprpaper stowed"
+stow backgrounds
+GREP_COLORS='mt=1;36' echo "[+] backgrounds stowed"
 
 # Terminal configuration
 GREP_COLORS='mt=1;36' echo "\n Configuring kitty terminal"
+stow kitty
+GREP_COLORS='mt=1;36' echo "[+] kitty stowed"
+GREP_COLORS='mt=1;36' echo "Installing zsh"
+sudo pacman -Sy zsh
+zsh
+chsh -s `which zsh`
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
