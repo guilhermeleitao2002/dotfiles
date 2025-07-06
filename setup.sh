805a7eeabaaf31ac93c355c565bfdf565fd25ec0
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-07-06 15:53:21 +0100

    feat: add status bar configuration

--- setup.sh
+++ setup.sh
@@ -21,3 +21,10 @@ chsh -s `which zsh`
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Status bar configuration
+GREP_COLORS='mt=1;36' echo "\n Configuring waybar status bar"
+killall waybar
+rm ~/.config/waybar/*
+stow waybar
+GREP_COLORS='mt=1;36' echo "[+] waybar stowed"
